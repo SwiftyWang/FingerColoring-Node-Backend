34e04e1387ed9eded195ded2e3f8b17b9bdec60b
Swifty Wang <wsk900906@gmail.com>
2016-11-26 21:07:56 +0800

    create project

---
+++ test.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+var port = 3001;
+
+app.param("name", function(req, res, next, id){
+    console.log("here is name");
+    console.log(req + res + next+ id);
+});
+
+app.get('/api/', function (req, res, next) {
+    console.log('although this matches');
+    next();
+});
+
+app.get("/api/", function (req, res) {
+    console.log(req.address);
+    res.send(req.param("name"));
+});
+
+app.get("/api/1", function (req, res) {
+    console.log(req.address);
+
+});
+
+app.get("/", function (req, res) {
+    console.log(req.body);
+    res.send("test0");
+});
+
+app.listen(port, function () {
+    console.log('Example app listening on '+port+'!');
+});
