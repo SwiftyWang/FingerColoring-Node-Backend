746eebc78acdcea2dc527d9048064dd9fd376a81
SwiftyWang <swifty.wang@tinklabs.com>
2016-12-02 17:13:46 +0800

    update.

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 
-var port = 3001;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.param("name", function(req, res, next, id){
     console.log("here is name");
@@ -28,6 +29,6 @@ app.get("/", function (req, res) {
     res.send("Service is available!");
 });
 
-app.listen(port, function () {
-    console.log('Example app listening on '+port+'!');
+app.listen(server_port,server_ip_address, function () {
+    console.log('Example app listening on '+server_ip_address + ':'+server_port+'!');
 });
